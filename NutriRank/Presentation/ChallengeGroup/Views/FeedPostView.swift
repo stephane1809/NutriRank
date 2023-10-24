1c4446a58db660c26200f730420c5cc9f4615a4b
Paulo Henrique <phsilva098@gmail.com>
2023-10-24 10:56:42 -0300

    hotfix: fixing navigation stacks

--- NutriRank/Presentation/ChallengeGroup/Views/FeedPostView.swift
+++ NutriRank/Presentation/ChallengeGroup/Views/FeedPostView.swift
@@ -39,130 +39,128 @@ public struct FeedPostView: View {
 
     public var body: some View {
         GeometryReader { metrics in
-            NavigationStack {
-                ZStack {
-                    Color(.defaultBackground)
-                        .ignoresSafeArea()
-                    VStack (alignment: .center, spacing: 20) {
+            ZStack {
+                Color(.defaultBackground)
+                    .ignoresSafeArea()
+                VStack (alignment: .center, spacing: 20) {
 
-                        Button {
-                            self.performNavigation.toggle()
-                        } label: {
-                            ZStack{
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .frame(width: 350, height: 137)
-                                            .overlay(
-                                                RoundedRectangle(cornerRadius: 7)
+                    Button {
+                        self.performNavigation.toggle()
+                    } label: {
+                        ZStack{
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .frame(width: 350, height: 137)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 7)
 //                                                    .stroke(Color.black, lineWidth: 1.5)
-                                            )
-                                            .foregroundColor(Color("FeedGroupHeaderColor"))
-                                            .shadow(radius: 1, x: 0, y: 1)
+                                        )
+                                        .foregroundColor(Color("FeedGroupHeaderColor"))
+                                        .shadow(radius: 1, x: 0, y: 1)
 
-                                        HStack{
-                                            Text(viewmodel.group.groupName)
-                                                .fontWeight(.bold)
-                                                .lineLimit(1)
-                                                .foregroundColor(.black)
-                                            Spacer()
-                                        }
-                                        .frame(width: 320)
-                                        .offset(x: 0, y: -40)
-
-                                        HStack{
-                                            Image(systemName: "flame.fill")
-                                                .foregroundColor(.black)
-                                            Text("Top 3:")
-                                                .bold()
-                                                .foregroundColor(.black)
-                                            Text("\(viewmodel.getNamePersonRanking(index: 0)), \(viewmodel.getNamePersonRanking(index: 1)), \(viewmodel.getNamePersonRanking(index: 2))")
-                                                .foregroundColor(.black)
-                                            Spacer()
-                                        }
-                                        .frame(width: 320)
-                                        .offset(x: 0, y: -3)
-
-                                        HStack{
-                                            Image(systemName: "clock")
-                                                .foregroundColor(.black)
-                                            Text("\(calendar.numberOfDaysBetween(start: todayDate, end: viewmodel.group.endDate)) dias restantes")
-                                                .foregroundColor(.black)
-                                            Spacer()
-                                        }
-                                        .frame(width: 320)
-                                        .offset(x: 0, y: 28)
+                                    HStack{
+                                        Text(viewmodel.group.groupName)
+                                            .fontWeight(.bold)
+                                            .lineLimit(1)
+                                            .foregroundColor(.black)
+                                        Spacer()
                                     }
-                        }
+                                    .frame(width: 320)
+                                    .offset(x: 0, y: -40)
 
-                        Button {
-                            self.isImagePickerDisplay.toggle()
+                                    HStack{
+                                        Image(systemName: "flame.fill")
+                                            .foregroundColor(.black)
+                                        Text("Top 3:")
+                                            .bold()
+                                            .foregroundColor(.black)
+                                        Text("\(viewmodel.getNamePersonRanking(index: 0)), \(viewmodel.getNamePersonRanking(index: 1)), \(viewmodel.getNamePersonRanking(index: 2))")
+                                            .foregroundColor(.black)
+                                        Spacer()
+                                    }
+                                    .frame(width: 320)
+                                    .offset(x: 0, y: -3)
+
+                                    HStack{
+                                        Image(systemName: "clock")
+                                            .foregroundColor(.black)
+                                        Text("\(calendar.numberOfDaysBetween(start: todayDate, end: viewmodel.group.endDate)) dias restantes")
+                                            .foregroundColor(.black)
+                                        Spacer()
+                                    }
+                                    .frame(width: 320)
+                                    .offset(x: 0, y: 28)
+                                }
+                    }
+
+                    Button {
+                        self.isImagePickerDisplay.toggle()
 //                            sheet = .selection
-                        } label: {
-                            Text("+ Nova Postagem")
-                                .font(.headline)
-                                .frame(width: 327, height: 35)
-                                .foregroundColor(.white)
-                        }
-                        .background(Color("FirstPlaceRanking"))
-                        .cornerRadius(10)
-                        .buttonStyle(.bordered)
+                    } label: {
+                        Text("+ Nova Postagem")
+                            .font(.headline)
+                            .frame(width: 327, height: 35)
+                            .foregroundColor(.white)
+                    }
+                    .background(Color("FirstPlaceRanking"))
+                    .cornerRadius(10)
+                    .buttonStyle(.bordered)
 
-                        VStack {
-                            Text("Postagens")
-                            if viewmodel.posts.isEmpty {
-                                Text("Não existem postagens no grupo.")
-                            } else {
-                                List(viewmodel.posts) { post in
-                                    Button {
-                                        self.isPostCardDisplay.toggle()
-                                    } label: {
-                                        CardPostView(title: post.title, memberName: post.owner!.name, createdDate: post.creationDate!, postImage: post.postImage!, userAvatar: post.owner!.avatar)
-                                    }.sheet(isPresented: $isPostCardDisplay){
-                                        SheetPostView(viewmodel: self.viewmodel, selectedImage: self.$selectedImage, post: post)
-                                    }
+                    VStack {
+                        Text("Postagens")
+                        if viewmodel.posts.isEmpty {
+                            Text("Não existem postagens no grupo.")
+                        } else {
+                            List(viewmodel.posts) { post in
+                                Button {
+                                    self.isPostCardDisplay.toggle()
+                                } label: {
+                                    CardPostView(title: post.title, memberName: post.owner!.name, createdDate: post.creationDate!, postImage: post.postImage!, userAvatar: post.owner!.avatar)
+                                }.sheet(isPresented: $isPostCardDisplay){
+                                    SheetPostView(viewmodel: self.viewmodel, selectedImage: self.$selectedImage, post: post)
                                 }
                             }
                         }
-                        Spacer()
                     }
-                    .confirmationDialog("Escolha uma opção", isPresented: $isImagePickerDisplay) {
-                        Button("Câmera") {
-                            self.sourceType = .camera
+                    Spacer()
+                }
+                .confirmationDialog("Escolha uma opção", isPresented: $isImagePickerDisplay) {
+                    Button("Câmera") {
+                        self.sourceType = .camera
+                        self.sheet = .selection
+                    }
+                    Button("Galeria") {
+                        if PHPhotoLibrary.authorizationStatus(for: .readWrite) == .authorized {
+                            self.sourceType = .photoLibrary
                             self.sheet = .selection
-                        }
-                        Button("Galeria") {
-                            if PHPhotoLibrary.authorizationStatus(for: .readWrite) == .authorized {
-                                self.sourceType = .photoLibrary
-                                self.sheet = .selection
-                            } else {
-                                PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
-                                    if status == .authorized {
-                                        self.sourceType = .photoLibrary
-                                        self.sheet = .selection
-                                    }
+                        } else {
+                            PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+                                if status == .authorized {
+                                    self.sourceType = .photoLibrary
+                                    self.sheet = .selection
                                 }
                             }
                         }
                     }
-                    .onChange(of: selectedImage) { selectedImage in
-                        if selectedImage != nil {
-                            sheet = .image
-                        }
+                }
+                .onChange(of: selectedImage) { selectedImage in
+                    if selectedImage != nil {
+                        sheet = .image
                     }
-                    .sheet(item: $sheet) { sheet in
-                        switch sheet {
-                            case .image:
-                            SheetCreatePostView(viewmodel: self.viewmodel, selectedImage: self.$selectedImage)
-                            case .selection:
-                            ImagePickerView(selectedImage: self.$selectedImage, sourceType: self.sourceType)
-                        }
+                }
+                .sheet(item: $sheet) { sheet in
+                    switch sheet {
+                        case .image:
+                        SheetCreatePostView(viewmodel: self.viewmodel, selectedImage: self.$selectedImage)
+                        case .selection:
+                        ImagePickerView(selectedImage: self.$selectedImage, sourceType: self.sourceType)
                     }
-                    .task {
-                        if self.viewmodel.group.record != nil {
-                            await viewmodel.fetchPosts()
-                        }
+                }
+                .task {
+                    if self.viewmodel.group.record != nil {
+                        await viewmodel.fetchPosts()
                     }
-                }.navigationDestination(isPresented: $performNavigation, destination: { GroupView(viewmodel: viewmodel) })
-            }
+                }
+            }.navigationDestination(isPresented: $performNavigation, destination: { GroupView(viewmodel: viewmodel) })
             .navigationBarBackButtonHidden(true)
         }
     }
